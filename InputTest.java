d308517550cc31322a7f1a317999d639a2f9d937
Vivek Ranjan <vivekranjan92@gmail.com>
2017-04-13 17:46:32 +0530

    Update InputTest.java

--- InputTest.java
+++ InputTest.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;  
 
-public class InputTest{  
-
- public static void main(String args[]){ 
-
-	   Scanner sc=new Scanner(System.in);  
-       
-   		System.out.print("Enter a number: ");
+public class InputTest
+{
+ public static void main(String args[])
+ {
+	Scanner sc=new Scanner(System.in);
+   	System.out.print("Enter a number: ");
     	int number = sc.nextInt();
 
-    if(number < 100){ 
+    if(number < 100)
+    { 
     	System.out.println("HELLO World");
-    	}
-    else {
+    }
+    else
+    {
     	System.out.println("BYE-BYE World");
-    	}
- 	}  
+    }
+ } 
 }
